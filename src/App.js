f8739c7263ae00b60870b4d0186a8474659c00f2
Max Goodman <max.goodman@flatironschool.com>
2019-03-04 10:53:04 -0500

    adding form and text balances and manager address

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import web3 from './Web3'
-import Lottery from './Lottery'; // can interact with the contract 
+import lottery from './Lottery'; // can interact with the contract 
 
 class App extends Component {
+  state = {
+    manager: "", 
+    players: [],
+    balance: "",
+    value: ""
+  }
+
+  async componentDidMount(){ //called whenever app component shows up on the screen
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({manager, players, balance});
+  }
+  
+  onSubmit=(e)=>{ //no need to bind the function with this syntax
+    e.preventDefault();
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>This is our Lottery contract</h2>
+        <p> This contract is managed by: {this.state.manager}</p>
+        <p>There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
+        <hr></hr>
+      <form onSubmit={this.onSubmit}>
+        <h4>Want to try your luck?</h4>
+          <div> 
+              <label>Amount of Ether to Enter: </label>
+              <input onChange={(e)=>this.setState({value: e.target.value })} value={this.state.value} />
+          </div>
+          <button>Enter</button>
+        </form>        
       </div>
     );
   }
